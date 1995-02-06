e1d500e3f201a265aa416ffee1818942676d8834
stadel <stadel>
1995-02-06 11:55:48 +0000

    Initial version of spx MDL. This has NOT been tested fully yet. This version is also missing the combiner cache, the addition of which should pose no major problems. Trickiest part is assurance of no deadlock possibilities in the coding of the cache. Also, the bprobe (which apparently has problems anyway) was not used. While the cache is active there is always an outstanding receive, the status of this receive is tested occasionally using mpc_status.

---
+++ mpl/mdl.h
@@ -0,0 +1,115 @@
+#ifndef MDL_HINCLUDED
+#define MDL_HINCLUDED
+#include <stdio.h>
+
+
+#define SRV_STOP		0
+
+typedef struct cacheTag {
+	int id;
+	int iLine;
+	int nLock;
+	int iLink;
+	} CTAG;
+
+
+/*
+ ** This structure should be "maximally" aligned, with 4 ints it
+ ** should align up to at least QUAD word, which should be enough.
+ */
+typedef struct cacheHeader {
+	int cid;
+	int mid;
+	int id;
+	int iLine;
+	} CAHEAD;
+
+
+typedef struct cacheSpace {
+	int iType;
+	char *pData;
+	int iDataSize;
+	int nData;
+	int nLineElts;
+	int iLineSize;
+	int nLines;
+	int nTrans;
+	int iTransMask;
+	int *pTrans;
+	CTAG *pTag;
+	char *pLine;
+	int nAccess;
+	int nCheckIn;
+	int nCheckOut;
+	CAHEAD caReq;
+	void (*init)(void *);
+	void (*combine)(void *,void *);
+	} CACHE;
+
+
+typedef struct serviceRec {
+	int nInBytes;
+	int nOutBytes;
+	void *p1;
+	void (*fcnService)(void *,void *,int,void *,int *);	
+	} SERVICE;
+
+
+typedef struct mdlContext {
+	int nThreads;
+	int idSelf;
+	int bDiag;
+	FILE *fpDiag;
+	int dontcare;
+	int allgrp;
+	/*
+	 ** Services stuff!
+	 */
+	int nMaxServices;
+	int nMaxSrvBytes;
+	SERVICE *psrv;
+	void *pvIn;
+	void *pvOut;
+	void *pvBuf;
+	/*
+	 ** Swapping buffer.
+	 */
+	char *pszBuf;
+	/*
+	 ** Caching stuff!
+	 */
+	unsigned long uRand;
+	int iMaxDataSize;
+	int iCaBufSize;
+	void *pvRcv;
+	int midRcv;
+	void **ppvRpl;
+	int nMaxCacheIds;
+	CACHE *cache;
+	} * MDL;
+
+
+double mdlCpuTimer(MDL);
+int mdlInitialize(MDL *,char **,void (*)(MDL));
+void mdlFinish(MDL);
+int mdlThreads(MDL);
+int mdlSelf(MDL);
+int mdlSwap(MDL,int,int,void *,int,int *,int *);
+void mdlDiag(MDL,char *);
+void mdlAddService(MDL,int,void *,void (*)(void *,void *,int,void *,int *),
+				   int,int);
+void mdlReqService(MDL,int,int,void *,int);
+void mdlGetReply(MDL,int,void *,int *);
+void mdlHandler(MDL);
+/*
+ ** Caching functions.
+ */
+void mdlROcache(MDL,int,void *,int,int);
+void mdlCOcache(MDL,int,void *,int,int,
+				void (*)(void *),void (*)(void *,void *));
+void mdlFinishCache(MDL,int);
+void *mdlAquire(MDL,int,int,int);
+void mdlRelease(MDL,int,void *);
+
+#endif
+
