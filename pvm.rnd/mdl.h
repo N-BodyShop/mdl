c6e6c0793fffd24bfe0aa71f90adc3b95723b1d2
stadel <stadel>
1995-07-06 21:05:09 +0000

    Least recently used cache. This version of MDL had the mdlMalloc and mdlFree functions, plus new form of cache statistics.

---
+++ pvm.rnd/mdl.h
@@ -0,0 +1,122 @@
+#ifndef MDL_HINCLUDED
+#define MDL_HINCLUDED
+#include <stdio.h>
+
+
+#define SRV_STOP		0
+
+#define MDL_CACHELINE_BITS	3
+#define MDL_CACHELINE_ELTS	(1<<MDL_CACHELINE_BITS)
+#define MDL_CACHE_MASK		(MDL_CACHELINE_ELTS-1)
+
+typedef struct cacheTag {
+	int iKey;
+	int id;
+	int nLock;
+	int iLink;
+	} CTAG;
+
+
+typedef struct cacheSpace {
+	int iType;
+	char *pData;
+	int iDataSize;
+	int nData;
+	int iLineSize;
+	int nLines;
+	int nTrans;
+	int iTransMask;
+	int *pTrans;
+	CTAG *pTag;
+	char *pLine;
+	int req[4];
+	int rpl[4];
+	int chko[4];
+	int chki[4];
+	int flsh[4];
+	int nCheckIn;
+	int nCheckOut;
+	void (*init)(void *);
+	void (*combine)(void *,void *);
+	/*	
+	 ** Statistics stuff.
+	 */
+	int nAccess;
+	int nAccHigh;
+	long nMiss;
+	long nColl;
+	long nMin;
+	int nKeyMax;
+	char *pbKey;
+	} CACHE;
+
+
+typedef struct serviceRec {
+	int nInBytes;
+	int nOutBytes;
+	void *p1;
+	void (*fcnService)(void *,void *,int,void *,int *);	
+	} SERVICE;
+
+
+typedef struct mdlContext {
+	int nThreads;
+	int idSelf;
+	int bDiag;
+	int *atid;
+	FILE *fpDiag;
+	/*
+	 ** Services stuff!
+	 */
+	int nMaxServices;
+	int nMaxInBytes;
+	int nMaxOutBytes;
+	SERVICE *psrv;
+	char *pszIn;
+	char *pszOut;
+	/*
+	 ** Caching stuff!
+	 */
+	unsigned long uRand;
+	int iMaxDataSize;
+	int nMaxCacheIds;
+	CACHE *cache;
+	} * MDL;
+
+
+double mdlCpuTimer(MDL);
+int mdlInitialize(MDL *,char **,void (*)(MDL));
+void mdlFinish(MDL);
+int mdlThreads(MDL);
+int mdlSelf(MDL);
+int mdlSwap(MDL,int,int,void *,int,int *,int *);
+void mdlDiag(MDL,char *);
+void mdlAddService(MDL,int,void *,void (*)(void *,void *,int,void *,int *),
+				   int,int);
+void mdlReqService(MDL,int,int,void *,int);
+void mdlGetReply(MDL,int,void *,int *);
+void mdlHandler(MDL);
+/*
+ ** Caching functions.
+ */
+void *mdlMalloc(MDL,int);
+void mdlFree(MDL,void *);
+void mdlROcache(MDL,int,void *,int,int);
+void mdlCOcache(MDL,int,void *,int,int,
+				void (*)(void *),void (*)(void *,void *));
+void mdlFinishCache(MDL,int);
+void mdlCacheCheck(MDL);
+void *mdlAquire(MDL,int,int,int);
+void mdlRelease(MDL,int,void *);
+/*
+ ** Cache statistics functions.
+ */
+double mdlNumAccess(MDL,int);
+double mdlMissRatio(MDL,int);
+double mdlCollRatio(MDL,int);
+double mdlMinRatio(MDL,int);
+
+#endif
+
+
+
