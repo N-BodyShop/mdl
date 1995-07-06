d783cb6e37682db854ee8a44c1ecd58b7dcb7c07
stadel <stadel>
1995-07-06 22:20:35 +0000

    T3D MDL, not supporting COcache yet.

---
+++ mpp/mdl.h
@@ -0,0 +1,108 @@
+#ifndef MDL_HINCLUDED
+#define MDL_HINCLUDED
+#include <stdio.h>
+
+
+#define SRV_STOP		0
+
+typedef struct cacheTag {
+	int iKey;
+	int nLock;
+	int nLast;
+	int iLink;
+	} CTAG;
+
+
+typedef struct cacheSpace {
+	int iType;
+	char *pData;
+	int iDataSize;
+	int nData;
+	int nLineElts;
+	int iLineSize;
+	int nLines;
+	int nTrans;
+	int iTransMask;
+	int *pTrans;
+	CTAG *pTag;
+	char *pLine;
+        long pDataMax;
+	/*	
+	 ** Statistics stuff.
+	 */
+	int nAccess;
+	int nAccHigh;
+	long nMiss;
+	long nColl;
+	long nMin;
+	} CACHE;
+
+
+
+typedef struct serviceRec {
+	int nInBytes;
+	int nOutBytes;
+	void *p1;
+	void (*fcnService)(void *,void *,int,void *,int *);	
+	} SERVICE;
+
+
+typedef struct mdlContext {
+	int nThreads;
+	int idSelf;
+	int bDiag;
+	int *atid;
+	FILE *fpDiag;
+	/*
+	 ** Services stuff!
+	 */
+	int nMaxServices;
+	int nMaxInBytes;
+	int nMaxOutBytes;
+	SERVICE *psrv;
+	char *pszIn;
+	char *pszOut;
+	/*
+	 ** Caching stuff!
+	 */
+	unsigned long uRand;
+	int iMaxDataSize;
+	int nMaxCacheIds;
+	CACHE *cache;
+	} * MDL;
+
+
+double mdlCpuTimer(MDL);
+int mdlInitialize(MDL *,char **,void (*)(MDL));
+void mdlFinish(MDL);
+int mdlThreads(MDL);
+int mdlSelf(MDL);
+int mdlSwap(MDL,int,int,void *,int,int *,int *);
+void mdlDiag(MDL,char *);
+void mdlAddService(MDL,int,void *,void (*)(void *,void *,int,void *,int *),
+				   int,int);
+void mdlReqService(MDL,int,int,void *,int);
+void mdlGetReply(MDL,int,void *,int *);
+void mdlHandler(MDL);
+/*
+ ** Caching functions.
+ */
+void *mdlMalloc(MDL,int);
+void mdlFree(MDL,void *);
+void mdlROcache(MDL,int,void *,int,int);
+void mdlCOcache(MDL,int,void *,int,int,
+				void (*)(void *),void (*)(void *,void *));
+void mdlFinishCache(MDL,int);
+void *mdlAquire(MDL,int,int,int);
+void mdlRelease(MDL,int,void *);
+
+/*
+ ** Cache statistics functions.
+ */
+double mdlNumAccess(MDL,int);
+double mdlMissRatio(MDL,int);
+double mdlCollRatio(MDL,int);
+double mdlMinRatio(MDL,int);
+
+#endif
+
