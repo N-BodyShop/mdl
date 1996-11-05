05b424b34f9256b41a7382b08184a3be08867fd6
trq <trq>
1996-11-05 17:16:31 +0000

    Created NULL mdl.

---
+++ null/mdl.c
@@ -0,0 +1,598 @@
+/*
+ ** NULL mdl.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <malloc.h>
+#include <math.h>
+#include <limits.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include "mdl.h"
+
+
+#define MDL_NOCACHE			0
+#define MDL_ROCACHE			1
+#define MDL_COCACHE			2
+
+
+#define MDL_DEFAULT_BYTES		4096
+#define MDL_DEFAULT_SERVICES	50
+#define MDL_DEFAULT_CACHEIDS	5
+
+#define PVM_TRANS_SIZE		50000 
+#define MDL_TAG_INIT 		1
+#define MDL_TAG_SWAPINIT 	2
+#define MDL_TAG_SWAP		3
+#define MDL_TAG_REQ	   		4
+#define MDL_TAG_RPL			5
+
+
+void _srvNull(void *p1,void *vin,int nIn,void *vout,int *pnOut)
+{
+	return;
+	}
+
+
+double mdlCpuTimer(MDL mdl)
+{
+	struct rusage ru;
+
+	getrusage(0,&ru);
+	return((double)ru.ru_utime.tv_sec + 1e-6*(double)ru.ru_utime.tv_usec);
+	}
+
+
+int mdlInitialize(MDL *pmdl,char **argv,void (*fcnChild)(MDL))
+{
+	MDL mdl;
+	int i,nThreads,bDiag,bThreads;
+	char *p,ach[256],achDiag[256];
+
+	*pmdl = NULL;
+	mdl = malloc(sizeof(struct mdlContext));
+	assert(mdl != NULL);
+	/*
+	 ** Set default "maximums" for structures. These are NOT hard
+	 ** maximums, as the structures will be realloc'd when these
+	 ** values are exceeded.
+	 */
+	mdl->nMaxServices = MDL_DEFAULT_SERVICES;
+	mdl->nMaxInBytes = MDL_DEFAULT_BYTES;
+	mdl->nMaxOutBytes = MDL_DEFAULT_BYTES;
+	mdl->nMaxCacheIds = MDL_DEFAULT_CACHEIDS;
+	/*
+	 ** Now allocate the initial service slots.
+	 */
+	mdl->psrv = malloc(mdl->nMaxServices*sizeof(SERVICE));
+	assert(mdl->psrv != NULL);
+	/*
+	 ** Initialize the new service slots.
+	 */
+	for (i=0;i<mdl->nMaxServices;++i) {
+		mdl->psrv[i].p1 = NULL;
+		mdl->psrv[i].nInBytes = 0;
+		mdl->psrv[i].nOutBytes = 0;
+		mdl->psrv[i].fcnService = NULL;
+		}
+	/*
+	 ** Provide a 'null' service for sid = 0, so that stopping the 
+	 ** service handler is well defined!
+	 */
+	mdl->psrv[0].p1 = NULL;
+	mdl->psrv[0].nInBytes = 0;
+	mdl->psrv[0].nOutBytes = 0;
+	mdl->psrv[0].fcnService = _srvNull;
+	/*
+	 ** Allocate service buffers.
+	 */
+	mdl->pszIn = malloc(mdl->nMaxInBytes);
+	assert(mdl->pszIn != NULL);
+	mdl->pszOut = malloc(mdl->nMaxOutBytes);
+	assert(mdl->pszOut != NULL);
+	/*
+	 ** Allocate initial cache spaces.
+	 */
+	mdl->cache = malloc(mdl->nMaxCacheIds*sizeof(CACHE));
+	assert(mdl->cache != NULL);
+	/*
+	 ** Initialize caching spaces.
+	 */
+	for (i=0;i<mdl->nMaxCacheIds;++i) {
+		mdl->cache[i].iType = MDL_NOCACHE;
+		}
+	/*
+	 ** Do some low level argument parsing for number of threads, and
+	 ** diagnostic flag!
+	 */
+	bDiag = 0;
+	bThreads = 0;
+	i = 1;
+	while (argv[i]) {
+		if (!strcmp(argv[i],"-sz") && !bThreads) {
+			++i;
+			if (argv[i]) {
+				nThreads = atoi(argv[i]);
+				bThreads = 1;
+				}
+			}
+		if (!strcmp(argv[i],"+d") && !bDiag) {
+			p = getenv("MDL_DIAGNOSTIC");
+			if (!p) p = getenv("HOME");
+			if (!p) sprintf(ach,"/tmp");
+			else sprintf(ach,"%s",p);
+			bDiag = 1;
+			}
+		++i;
+		}
+	nThreads = 1;
+	mdl->bDiag = bDiag;
+	mdl->nThreads = nThreads;
+	*pmdl = mdl;
+		/*
+		 ** A unik!
+		 */
+		mdl->idSelf = 0;
+		if (mdl->bDiag) {
+			sprintf(achDiag,"%s.%d",ach,mdl->idSelf);
+			mdl->fpDiag = fopen(achDiag,"w");
+			assert(mdl->fpDiag != NULL);
+			}
+	return(nThreads);
+	}
+
+
+void mdlFinish(MDL mdl)
+{
+	/*
+	 ** Close Diagnostic file.
+	 */
+	if (mdl->bDiag) {
+		fclose(mdl->fpDiag);
+		}
+	/*
+	 ** Deregister from PVM and deallocate storage.
+	 */
+	free(mdl->psrv);
+	free(mdl->pszIn);
+	free(mdl->pszOut);
+	free(mdl->cache);
+	free(mdl->atid);
+	free(mdl);
+	}
+
+
+/*
+ ** This function returns the number of threads in the set of 
+ ** threads.
+ */
+int mdlThreads(MDL mdl)
+{
+	return(mdl->nThreads);
+	}
+
+
+/*
+ ** This function returns this threads 'id' number within the specified
+ ** MDL Context. Parent thread always has 'id' of 0, where as children
+ ** have 'id's ranging from 1..(nThreads - 1).
+ */
+int mdlSelf(MDL mdl)
+{
+	return(mdl->idSelf);
+	}
+
+
+/*
+ ** This is a tricky function. It initiates a bilateral transfer between
+ ** two threads. Both threads MUST be expecting this transfer. The transfer
+ ** occurs between idSelf <---> 'id' or 'id' <---> idSelf as seen from the
+ ** opposing thread. It is designed as a high performance non-local memory
+ ** swapping primitive and implementation will vary in non-trivial ways 
+ ** between differing architectures and parallel paradigms (eg. message
+ ** passing and shared address space). A buffer is specified by 'pszBuf'
+ ** which is 'nBufBytes' in size. Of this buffer the LAST 'nOutBytes' are
+ ** transfered to the opponent, in turn, the opponent thread transfers his
+ ** nBufBytes to this thread's buffer starting at 'pszBuf'.
+ ** If the transfer completes with no problems the function returns 1.
+ ** If the function returns 0 then one of the players has not received all
+ ** of the others memory, however he will have successfully transfered all
+ ** of his memory.
+ */
+int mdlSwap(MDL mdl,int id,int nBufBytes,void *vBuf,int nOutBytes,
+			int *pnSndBytes,int *pnRcvBytes)
+{
+	assert(0);
+	}
+
+
+void mdlDiag(MDL mdl,char *psz)
+{
+	if (mdl->bDiag) {	
+		fputs(psz,mdl->fpDiag);
+		fflush(mdl->fpDiag);
+		}
+	}
+
+
+void mdlAddService(MDL mdl,int sid,void *p1,
+				   void (*fcnService)(void *,void *,int,void *,int *),
+				   int nInBytes,int nOutBytes)
+{
+	int i,nMaxServices;
+
+	assert(sid > 0);
+	if (sid >= mdl->nMaxServices) {
+		/*
+		 ** reallocate service buffer, adding space for 8 new services
+		 ** including the one just defined.
+		 */
+		nMaxServices = sid + 9;
+		mdl->psrv = realloc(mdl->psrv,nMaxServices*sizeof(SERVICE));
+		assert(mdl->psrv != NULL);
+		/*
+		 ** Initialize the new service slots.
+		 */
+		for (i=mdl->nMaxServices;i<nMaxServices;++i) {
+			mdl->psrv[i].p1 = NULL;
+			mdl->psrv[i].nInBytes = 0;
+			mdl->psrv[i].nOutBytes = 0;
+			mdl->psrv[i].fcnService = NULL;
+			}
+		mdl->nMaxServices = nMaxServices;
+		}
+	/*
+	 ** Make sure the service buffers are big enough!
+	 */
+	if (nInBytes > mdl->nMaxInBytes) {
+		mdl->pszIn = realloc(mdl->pszIn,nInBytes);
+		assert(mdl->pszIn != NULL);
+		mdl->nMaxInBytes = nInBytes;
+		}
+	if (nOutBytes > mdl->nMaxOutBytes) {
+		mdl->pszOut = realloc(mdl->pszOut,nOutBytes);
+		assert(mdl->pszOut != NULL);
+		mdl->nMaxOutBytes = nOutBytes;
+		}
+	mdl->psrv[sid].p1 = p1;
+	mdl->psrv[sid].nInBytes = nInBytes;
+	mdl->psrv[sid].nOutBytes = nOutBytes;
+	mdl->psrv[sid].fcnService = fcnService;
+	}
+
+
+void mdlReqService(MDL mdl,int id,int sid,void *vin,int nInBytes)
+{
+	assert(0);
+	}
+
+
+void mdlGetReply(MDL mdl,int id,void *vout,int *pnOutBytes)
+{
+	assert(0);
+	}
+
+
+void mdlHandler(MDL mdl)
+{
+	assert(0);
+	}
+
+
+#define MDL_TAG_CACHECOM	10
+#define MDL_MID_CACHEIN		1
+#define MDL_MID_CACHEREQ	2
+#define MDL_MID_CACHERPL	3
+#define MDL_MID_CACHEOUT	4
+#define MDL_MID_CACHEFLSH	5
+
+#define MDL_CHECK_MASK  	0x7f
+#define BILLION				1000000000
+
+
+void AdjustDataSize(MDL mdl)
+{
+	int i,iMaxDataSize;
+
+	/*
+	 ** Change buffer size?
+	 */
+	iMaxDataSize = 0;
+	for (i=0;i<mdl->nMaxCacheIds;++i) {
+		if (mdl->cache[i].iType == MDL_NOCACHE) continue;
+		if (mdl->cache[i].iDataSize > iMaxDataSize) {
+			iMaxDataSize = mdl->cache[i].iDataSize;
+			}
+		}
+	if (iMaxDataSize != mdl->iMaxDataSize) {
+		/*
+		 ** Create new buffer with realloc?
+		 ** Be very careful when reallocing buffers in other libraries
+		 ** (not PVM) to be sure that the buffers are not in use!
+		 ** A pending non-blocking receive on a buffer which is realloced
+		 ** here will cause problems, make sure to take this into account!
+		 */
+		mdl->iMaxDataSize = iMaxDataSize;
+		}
+	}
+
+
+/*
+ ** Special MDL memory allocation functions for allocating memory 
+ ** which must be visible to other processors thru the MDL cache 
+ ** functions.
+ ** mdlMalloc() is defined to return a pointer to AT LEAST iSize bytes 
+ ** of memory. This pointer will be passed to either mdlROcache or 
+ ** mdlCOcache as the pData parameter.
+ ** For PVM and most machines these functions are trivial, but on the 
+ ** T3D and perhaps some future machines these functions are required.
+ */
+void *mdlMalloc(MDL mdl,int iSize)
+{	
+	return(malloc(iSize));
+	}
+
+
+void mdlFree(MDL mdl,void *p)
+{
+	free(p);
+	}
+
+
+/*
+ ** Common initialization for all types of caches.
+ */
+CACHE *CacheInitialize(MDL mdl,int cid,void *pData,int iDataSize,int nData)
+{
+	CACHE *c;
+	int i,nMaxCacheIds;
+
+	/*
+	 ** Allocate more cache spaces if required!
+	 */
+	assert(cid >= 0);
+	if (cid >= mdl->nMaxCacheIds) {
+		/*
+		 ** reallocate cache spaces, adding space for 2 new cache spaces
+		 ** including the one just defined.
+		 */
+		nMaxCacheIds = cid + 3;
+		mdl->cache = realloc(mdl->cache,nMaxCacheIds*sizeof(CACHE));
+		assert(mdl->cache != NULL);
+		/*
+		 ** Initialize the new cache slots.
+		 */
+		for (i=mdl->nMaxCacheIds;i<nMaxCacheIds;++i) {
+			mdl->cache[i].iType = MDL_NOCACHE;
+			}
+		mdl->nMaxCacheIds = nMaxCacheIds;
+		}
+	c = &mdl->cache[cid];
+	assert(c->iType == MDL_NOCACHE);
+	c->pData = pData;
+	c->iDataSize = iDataSize;
+	c->nData = nData;
+	c->iLineSize = MDL_CACHELINE_ELTS*c->iDataSize;
+	/*
+	 ** Determine the number of cache lines to be allocated.
+	 */
+	c->nLines = (MDL_CACHE_SIZE/c->iDataSize) >> MDL_CACHELINE_BITS;
+	c->nTrans = 1;
+	while(c->nTrans < c->nLines) c->nTrans *= 2;
+	c->iTransMask = c->nTrans-1;
+	/*
+	 **	Set up the translation table.
+	 */
+	c->pTrans = malloc(c->nTrans*sizeof(int));	
+	assert(c->pTrans != NULL);
+	for (i=0;i<c->nTrans;++i) c->pTrans[i] = 0;
+	/*
+	 ** Set up the tags. Note pTag[0] is a Sentinel!
+	 */
+	c->pTag = malloc(c->nLines*sizeof(CTAG));
+	assert(c->pTag != NULL);
+	for (i=0;i<c->nLines;++i) {
+		c->pTag[i].iKey = -1;	/* invalid */	
+		c->pTag[i].nLock = 0;
+		c->pTag[i].nLast = 0;
+		c->pTag[i].iLink = 0;
+		}
+	c->pTag[0].nLock = 1;			/* always locked */
+	c->pTag[0].nLast = INT_MAX;  	/* always Most Recently Used */
+	c->nAccess = 0;
+	c->nAccHigh = 0;
+	c->nMiss = 0;
+	c->nColl = 0;
+	c->nMin = 0;
+	c->nKeyMax = 500;
+	c->pbKey = malloc(c->nKeyMax);
+	assert(c->pbKey != NULL);
+	for (i=0;i<c->nKeyMax;++i) c->pbKey[i] = 0;
+	/*
+	 ** Allocate cache data lines.
+	 */
+	c->pLine = malloc(c->nLines*c->iLineSize);
+	assert(c->pLine != NULL);
+	c->req[0] = cid;
+	c->req[1] = MDL_MID_CACHEREQ;
+	c->req[2] = mdl->idSelf;
+	c->rpl[0] = cid;
+	c->rpl[1] = MDL_MID_CACHERPL;
+	c->rpl[2] = mdl->idSelf;
+	c->chko[0] = cid;
+	c->chko[1] = MDL_MID_CACHEOUT;
+	c->chko[2] = mdl->idSelf;
+	c->chki[0] = cid;
+	c->chki[1] = MDL_MID_CACHEIN;
+	c->chki[2] = mdl->idSelf;
+	c->flsh[0] = cid;
+	c->flsh[1] = MDL_MID_CACHEFLSH;
+	c->flsh[2] = mdl->idSelf;
+	c->nCheckIn = 0;
+	c->nCheckOut = 0;
+	return(c);
+	}
+
+
+/*
+ ** Initialize a Read-Only caching space.
+ */
+void mdlROcache(MDL mdl,int cid,void *pData,int iDataSize,int nData)
+{
+	CACHE *c;
+
+	c = CacheInitialize(mdl,cid,pData,iDataSize,nData);
+	c->iType = MDL_ROCACHE;
+	c->init = NULL;
+	c->combine = NULL;
+	AdjustDataSize(mdl);
+	}
+
+
+/*
+ ** Initialize a Combiner caching space.
+ */
+void mdlCOcache(MDL mdl,int cid,void *pData,int iDataSize,int nData,
+				void (*init)(void *),void (*combine)(void *,void *))
+{
+	CACHE *c;
+
+	c = CacheInitialize(mdl,cid,pData,iDataSize,nData);
+	c->iType = MDL_COCACHE;
+	c->init = init;
+	c->combine = combine;
+	AdjustDataSize(mdl);
+	}
+
+
+void mdlFinishCache(MDL mdl,int cid)
+{
+	CACHE *c = &mdl->cache[cid];
+
+	/*
+	 ** Free up storage and finish.
+	 */
+	free(c->pbKey);
+	free(c->pTrans);
+	free(c->pTag);
+	free(c->pLine);
+	c->iType = MDL_NOCACHE;
+	AdjustDataSize(mdl);
+	}
+
+void mdlCacheCheck(MDL mdl)
+{
+}
+
+void *mdlAquire(MDL mdl,int cid,int iIndex,int id)
+{
+	CACHE *c = &mdl->cache[cid];
+	char *pLine;
+	int iElt,iLine,i,iKey;
+
+	++c->nAccess;
+	/*
+	 ** Determine memory block key value and cache line.
+	 */
+	iLine = iIndex >> MDL_CACHELINE_BITS;
+	iKey = iLine*mdl->nThreads + id;
+	/*
+	 ** Consider the following:
+	 ** iKey = (iIndex << c->iKeyShift) | id;
+	 */
+	i = c->pTrans[iKey & c->iTransMask];
+	/*
+	 ** Check for a match!
+	 */
+	if (c->pTag[i].iKey == iKey) {
+		++c->pTag[i].nLock;
+		c->pTag[i].nLast = c->nAccess;
+		pLine = &c->pLine[i*c->iLineSize];
+		iElt = iIndex & MDL_CACHE_MASK;
+		return(&pLine[iElt*c->iDataSize]);
+		}
+	i = c->pTag[i].iLink;
+	/*
+	 ** Collision chain search.
+	 */
+	while (i) {
+		++c->nColl;
+		if (c->pTag[i].iKey == iKey) {
+			++c->pTag[i].nLock;
+			c->pTag[i].nLast = c->nAccess;
+			pLine = &c->pLine[i*c->iLineSize];
+			iElt = iIndex & MDL_CACHE_MASK;
+			return(&pLine[iElt*c->iDataSize]);
+			}
+		i = c->pTag[i].iLink;
+		}
+	/*
+	 ** Is it a local request?
+	 */
+	if (id == mdl->idSelf) {
+		return(&c->pData[iIndex*c->iDataSize]);
+		}
+	assert(0);
+	}
+
+
+void mdlRelease(MDL mdl,int cid,void *p)
+{
+	CACHE *c = &mdl->cache[cid];
+	int iLine,iData;
+	
+	iLine = ((char *)p - c->pLine) / c->iLineSize;
+	/*
+	 ** Check if the pointer fell in a cache line, otherwise it
+	 ** must have been a local pointer.
+	 */
+	if (iLine > 0 && iLine < c->nLines) {
+		--c->pTag[iLine].nLock;
+		assert(c->pTag[iLine].nLock >= 0);
+		}
+	else {
+		iData = ((char *)p - c->pData) / c->iDataSize;
+		assert(iData >= 0 && iData < c->nData);
+		}
+	}
+
+
+double mdlNumAccess(MDL mdl,int cid)
+{
+	CACHE *c = &mdl->cache[cid];
+
+	return(c->nAccHigh*1e9 + c->nAccess);
+	}
+
+
+double mdlMissRatio(MDL mdl,int cid)
+{
+	CACHE *c = &mdl->cache[cid];
+	double dAccess = c->nAccHigh*1e9 + c->nAccess;
+	
+	if (dAccess > 0.0) return(c->nMiss/dAccess);
+	else return(0.0);
+	}
+
+
+double mdlCollRatio(MDL mdl,int cid)
+{
+	CACHE *c = &mdl->cache[cid];
+	double dAccess = c->nAccHigh*1e9 + c->nAccess;
+
+	if (dAccess > 0.0) return(c->nColl/dAccess);
+	else return(0.0);
+	}
+
+
+double mdlMinRatio(MDL mdl,int cid)
+{
+	CACHE *c = &mdl->cache[cid];
+	double dAccess = c->nAccHigh*1e9 + c->nAccess;
+
+	if (dAccess > 0.0) return(c->nMin/dAccess);
+	else return(0.0);
+	}
