a8332b313f5493a5b9a5ce6616fa243fe400235a
trq <trq>
2005-06-23 13:52:15 +0000

    Updated definition of mdlSwap() for large memory problems.

--- null/mdl.c
+++ null/mdl.c
@@ -274,8 +274,8 @@ int mdlSelf(MDL mdl)
  ** of the others memory, however he will have successfully transfered all
  ** of his memory.
  */
-int mdlSwap(MDL mdl,int id,int nBufBytes,void *vBuf,int nOutBytes,
-			int *pnSndBytes,int *pnRcvBytes)
+int mdlSwap(MDL mdl,int id,size_t nBufBytes,void *vBuf,size_t nOutBytes,
+			size_t *pnSndBytes,size_t *pnRcvBytes)
 {
 	assert(0);
 	return 0;
